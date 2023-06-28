f898e6e725da86d0db1cb216f28befefc2654216
Maksim_Tataurov <maksimuspiter@gmail.com>
2023-06-28 18:50:36 +0300

    ref style

--- project/frontend/src/components/global/WorkExamples.vue
+++ project/frontend/src/components/global/WorkExamples.vue
@@ -4,22 +4,22 @@
 <template>
     <div class="row">
         <div class="col">
-            <div class="exaple-item mb-2" style="background-color: brown; height: 300px;"></div>
+            <div class="exaple-item mb-4" style="background-color: brown; height: 300px;"></div>
             <div class="exaple-item" style="background-color: rgb(42, 165, 42); height: 150px;"></div>
         </div>
         <div class="col">
-            <div class="exaple-item  mb-1" style="background-color: rgb(42, 104, 165); height: 150px;"></div>
-            <div class="exaple-item  mb-1" style="background-color: rgb(116, 6, 6); height: 150px;"></div>
+            <div class="exaple-item  mb-2" style="background-color: rgb(42, 104, 165); height: 150px;"></div>
+            <div class="exaple-item  mb-2" style="background-color: rgb(116, 6, 6); height: 150px;"></div>
             <div class="exaple-item" style="background-color: rgb(42, 165, 118); height: 150px;"></div>
 
         </div>
         <div class="col">
-            <div class="exaple-item  mb-1" style="background-color: rgb(45, 76, 108); height: 150px;"></div>
-            <div class="exaple-item  mb-1" style="background-color: rgb(42, 165, 81); height: 150px;"></div>
+            <div class="exaple-item  mb-2" style="background-color: rgb(45, 76, 108); height: 150px;"></div>
+            <div class="exaple-item  mb-2" style="background-color: rgb(42, 165, 81); height: 150px;"></div>
             <div class="exaple-item" style="background-color: rgb(165, 42, 99); height: 150px;"></div>
         </div>
         <div class="col">
-            <div class="exaple-item  mb-2" style="background-color: rgb(20, 106, 149); height: 300px;"></div>
+            <div class="exaple-item  mb-4" style="background-color: rgb(20, 106, 149); height: 300px;"></div>
             <div class="exaple-item" style="background-color: rgb(42, 165, 42); height: 150px;"></div>
         </div>
 
