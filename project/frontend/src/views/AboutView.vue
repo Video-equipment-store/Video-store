db54d7714e855016a94bba7ea6d72462fdcd128b
Maksim_Tataurov <maksimuspiter@gmail.com>
2023-07-28 13:19:06 +0300

    add about view

--- project/frontend/src/views/AboutView.vue
+++ project/frontend/src/views/AboutView.vue
@@ -1,8 +1,30 @@
+<script setup>
+import { ref, reactive } from 'vue'
+
+const objectOfAttrs = ref({
+  id: 'container',
+  class: 'wrapper'
+})
+const off = () => {
+  objectOfAttrs.value = false
+  console.log(objectOfAttrs)
+}
+const message = "hello world"
+const seen = ref(true)
+</script>
+
 <template>
   <main>
-    <h4>About view</h4>
-    <div>
-      <h5>Example global component: </h5>
+    <div class="d-flex justify-content-center">
+      <div>
+        <h4 class="text-center">About view</h4>
+        {{ message.split('').reverse().join('') }}
+        <!-- <h3>{{ new Date(2011, 0, 1, 0, 0, 0, 0) }}</h3> -->
+        <button @click="seen = !seen">seen</button>
+        <p v-if="seen">Now you see me</p>
+
+      </div>
+
     </div>
 
   </main>
