8ffd53859e3dd9ffb0bdf45e776dee2876bad799
Maksim_Tataurov <maksimuspiter@gmail.com>
2023-07-29 09:21:11 +0300

    del old apps

--- project/backend/goods/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from goods.models import Good
-
-
-@admin.register(Good)
-class GoodAdmin(admin.ModelAdmin):
-    list_display = ["title", "min_price", "price_coefficient", "available"]
-    list_editable = ["min_price", "price_coefficient", "available"]
-    list_filter = ["available"]
--- project/backend/services/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from services.models import Service
-
-
-@admin.register(Service)
-class ServiceAdmin(admin.ModelAdmin):
-    list_display = ["title", "min_price"]
-    list_editable = ["min_price"]
-    # list_filter = ["available"]
